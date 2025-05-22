971891bb0f45b8b50719250adc867df0fd653816
hsrokon <hsrokonofficial@gmail.com>
2025-05-22 23:33:12 +0600

    single coffee details and delete handled

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -37,6 +37,19 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/coffee/:id', async(req,res)=> {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id)};
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.delete('/coffee/:id', async(req,res)=> {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
