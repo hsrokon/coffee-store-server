88c93ab9b760eb2fda4fdd73df6c7d3260203609
hsrokon <hsrokonofficial@gmail.com>
2025-05-24 00:33:59 +0600

    user get and delete request handled

--- index.js
+++ index.js
@@ -80,6 +80,18 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/users', async(req, res)=> {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
